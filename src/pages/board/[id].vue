6701ba4ce1eaf400a85dc69af9d3aa45ba1440e9
vccyb <13170906656@163.com>
2025-05-05 16:34:21 +0800

    feat: 123

--- src/pages/board/[id].vue
+++ src/pages/board/[id].vue
@@ -4,6 +4,11 @@
         <participants />
         <toolbar :canvasState="canvasState" :setCanvasState="setCanvasState" :canRedo="false" :canUndo="false"
             :undo="() => { }" :redo="() => { }" />
+        <svg class="h-[100vh] w-[100vw]" @wheel="handleWheel">
+            <g :style="{ transform: `translate(${camera.x}px,${camera.y}px)` }">
+                <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)" />
+            </g>
+        </svg>
     </div>
 </template>
 
@@ -15,15 +20,36 @@ import { useRoute } from 'vue-router'
 import { ref } from 'vue'
 
 
-import type { CanvasState } from "@/types/canvas"
+import type { CanvasState, Camera } from "@/types/canvas"
 import { CanvasMode } from "@/types/canvas"
 const canvasState = ref<CanvasState>({
     mode: CanvasMode.None
 })
 const setCanvasState = (state: CanvasState) => {
-    canvasState.value = state
+
 }
 
 const route = useRoute()
 const id = route.params.id
+
+
+
+/** camera */
+
+const camera = ref<Camera>({
+    x: 0,
+    y: 0,
+})
+
+const setCamera = (newCamera: Camera) => {
+    camera.value = newCamera
+}
+
+const handleWheel = (event: WheelEvent) => {
+    const newCamera = {
+        x: camera.value.x - event.deltaX,
+        y: camera.value.y - event.deltaY,
+    }
+    setCamera(newCamera)
+}
 </script>
